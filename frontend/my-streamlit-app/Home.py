c82cc7203fe5e656663fdaa838338b1daf5b31f5
Grancho <guilhermegranchopro@gmail.com>
2025-04-23 04:18:05 +0100

    Refactor code for improved readability and consistency across multiple files
    - Standardized environment variable assignments in Home.py and Predictions.py
    - Enhanced code formatting for better readability in Academic_References.py, Model_Stats.py, Overview.py, and Predictions.py
    - Consolidated return statements in get_paths function for clarity in Predictions.py
    - Improved comments and structure in main_predictions function for better understanding

--- frontend/my-streamlit-app/Home.py
+++ frontend/my-streamlit-app/Home.py
@@ -11,14 +11,17 @@ devnull = os.open(os.devnull, os.O_WRONLY)
 os.dup2(devnull, sys.stderr.fileno())
 os.close(devnull)
 # Disable GPU and suppress TF/Absl logs
-os.environ['CUDA_VISIBLE_DEVICES'] = ''
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-os.environ['TF_CPP_MIN_VLOG_LEVEL'] = '3'
-os.environ['ABSL_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "3"
+os.environ["ABSL_CPP_MIN_LOG_LEVEL"] = "3"
+
 
 def main():
     # Set the page configuration before any UI elements
-    st.set_page_config(layout="wide", page_title="Prometheus - Quant Research", page_icon="🔥")
+    st.set_page_config(
+        layout="wide", page_title="Prometheus - Quant Research", page_icon="🔥"
+    )
 
     # Page Title
     st.title("Prometheus")
