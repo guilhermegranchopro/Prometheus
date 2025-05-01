e02c35b9320bc432cbe4dd5e6726db7b86ccc89d
Grancho <guilhermegranchopro@gmail.com>
2025-05-01 21:53:56 +0100

    refactor(Update File Paths): :bug: update file paths from models and scalers
    Update every model and scaler

--- frontend/my-streamlit-app/backend/Predictions.py
+++ frontend/my-streamlit-app/backend/Predictions.py
@@ -77,72 +77,60 @@ def get_paths(stock_ID):
     scaler_file_extension = ".pkl"
 
     if stock_ID == "VOO":
+        scaler_name = "Robust"
+        scaler_filename = (
+            "robust_ds=sip+s=VOO+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min"
+            + scaler_file_extension
+        )
         model_filename = (
-            "ds=sip+s=VOO+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.31953105330467224+ta=0.8718245029449463"
+            "ds=sip+s=VOO+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=robust+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3559+ta=0.8741"
             + model_file_extension
         )
-        model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
-        return model_path, None
+        scaler_path = os.path.join(DATA_BASE_PATH, scaler_name, scaler_filename)
+        model_path = os.path.join(MODELS_BASE_PATH, scaler_name, model_filename)
+        return model_path, scaler_path
     elif stock_ID == "DIA":
         model_filename = (
-            "ds=sip+s=DIA+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.36404281854629517+ta=0.8434399366378784"
+            "ds=sip+s=DIA+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3716+ta=0.8385"
             + model_file_extension
         )
         model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
         return model_path, None
     elif stock_ID == "IWM":
         model_filename = (
-            "ds=sip+s=IWM+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.39144110679626465+ta=0.832350492477417"
+            "ds=sip+s=IWM+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3888+ta=0.8239"
             + model_file_extension
         )
         model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
         return model_path, None
     elif stock_ID == "NVDA":
-        scaler_name = "Robust"
-        scaler_filename = (
-            "robust_ds=sip+s=NVDA+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min"
-            + scaler_file_extension
-        )
         model_filename = (
-            "ds=sip+s=NVDA+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=robust+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.6547278761863708+ta=0.7020547986030579"
+            "ds=sip+s=NVDA+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3756+ta=0.8445"
             + model_file_extension
         )
-        model_path = os.path.join(MODELS_BASE_PATH, scaler_name, model_filename)
-        scaler_path = os.path.join(DATA_BASE_PATH, scaler_name, scaler_filename)
-        return model_path, scaler_path
+        model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
+        return model_path, None
     elif stock_ID == "AAPL":
-        scaler_name = "MinMax"
-        scaler_filename = (
-            "minmax_ds=sip+s=AAPL+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min"
-            + scaler_file_extension
-        )
         model_filename = (
-            "ds=sip+s=AAPL+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=minmax+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.41606152057647705+ta=0.8365758657455444"
+            "ds=sip+s=AAPL+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.4137+ta=0.8440"
             + model_file_extension
         )
-        model_path = os.path.join(MODELS_BASE_PATH, scaler_name, model_filename)
-        scaler_path = os.path.join(DATA_BASE_PATH, scaler_name, scaler_filename)
-        return model_path, scaler_path
+        model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
+        return model_path, None
     elif stock_ID == "MSFT":
         model_filename = (
-            "ds=sip+s=MSFT+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3996354639530182+ta=0.8395842909812927"
+            "ds=sip+s=MSFT+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.3464+ta=0.8599"
             + model_file_extension
         )
         model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
         return model_path, None
     elif stock_ID == "AMZN":
-        scaler_name = "Robust"
-        scaler_filename = (
-            "robust_ds=sip+s=AMZN+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min"
-            + scaler_file_extension
-        )
         model_filename = (
-            "ds=sip+s=AMZN+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=robust+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.475951611995697+ta=0.8117924332618713"
+            "ds=sip+s=AMZN+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=none+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.6051+ta=0.8"
             + model_file_extension
         )
-        model_path = os.path.join(MODELS_BASE_PATH, scaler_name, model_filename)
-        scaler_path = os.path.join(DATA_BASE_PATH, scaler_name, scaler_filename)
-        return model_path, scaler_path
+        model_path = os.path.join(MODELS_BASE_PATH, "NoScaler", model_filename)
+        return model_path, None
     elif stock_ID == "GOOG":
         scaler_name = "Robust"
         scaler_filename = (
@@ -150,7 +138,7 @@ def get_paths(stock_ID):
             + scaler_file_extension
         )
         model_filename = (
-            "ds=sip+s=GOOG+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+rfm=False+rsm=False+rtm=False+d=+st=robust+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.539172351360321+ta=0.813238799571991"
+            "ds=sip+s=GOOG+mp=False+sd=2016-01-1+ed=2024-12-30+tf=5Min+fm=vwap+sm=trade_count+tm=+r=36+sort=False+adjustment=all+rfm=False+rsm=False+rtm=False+d=+st=robust+cts=[0]+Lb=True+e=500+es=True+cb=val_accuracy+p=100+bs=128+tl=0.577+ta=0.8156"
             + model_file_extension
         )
         model_path = os.path.join(MODELS_BASE_PATH, scaler_name, model_filename)
